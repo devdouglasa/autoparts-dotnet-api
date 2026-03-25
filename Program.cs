f4bc9414d770320468ad0c7ff4e9f0deccf352f9
devdouglasa <douglasalisson27@gmail.com>
2026-03-25 18:06:41 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using api_dotnet.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddScoped<ProductService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
