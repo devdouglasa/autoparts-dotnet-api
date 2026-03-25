08f545914126d255926dcd650da727a4afa99b69
devdouglasa <douglasalisson27@gmail.com>
2026-03-25 18:13:59 -0300

    feat: create ProductController for REST endpoints

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+using api_dotnet.Data;
+using api_dotnet.Models;
+using api_dotnet.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_dotnet.Controllers
+{
+    [Route("[controller]")]
+    public class ProductController(ProductService productService) : Controller
+    {
+        private readonly ProductService _productService = productService;
+
+        
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetAll()
+        {   
+            var products = await _productService.GetAll();
+
+            return products;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProductById(int id)
+        {
+            var product = await _productService.GetProductById(id);
+
+            if(product is null)
+            {
+                return NotFound("Produto não encontrado!");
+            }
+
+            return Ok(product);
+        }
+    }
+}
