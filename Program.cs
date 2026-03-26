a5a9e3ef10adb5eee6487a8408bfc35ff929c484
devdouglasa <douglasalisson27@gmail.com>
2026-03-26 12:00:39 -0300

    chore: register StockService and AppDbContext in Dependency Injection container

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddScoped<ProductService>();
+builder.Services.AddScoped<IStockService, StockService>();
 
 var app = builder.Build();
 
