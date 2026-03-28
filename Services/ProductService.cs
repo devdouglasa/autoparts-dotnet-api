ae5ca1d08da0bb1a263bfa5f352b08cf19c21f36
devdouglasa <douglasalisson27@gmail.com>
2026-03-28 10:42:44 -0300

    refactor: optimize ProductService using asynchronous projection and null safety

--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -1,5 +1,6 @@
 using api_dotnet.Data;
 using api_dotnet.Models;
+using api_dotnet.Dtos.Product;
 using Microsoft.EntityFrameworkCore;
 
 namespace api_dotnet.Services
@@ -8,16 +9,39 @@ namespace api_dotnet.Services
     {
         private readonly AppDbContext _appDbContext = appDbContext;
 
-        public async Task<List<Product>> GetAll()
+        public async Task<List<ProductResponseDto>> GetAll()
         {
-            var products = await _appDbContext.Products.ToListAsync();
+            var products = await _appDbContext.Products
+                .AsNoTracking()
+                .Select(p => new ProductResponseDto(
+                    p.Id,
+                    p.Name,
+                    p.Description,
+                    p.ManufacturerCode,
+                    p.Sku,
+                    p.Quantity,
+                    p.Price,
+                    p.CreatedAt
+                )).ToListAsync();
 
             return products;
         }
 
-        public async Task<Product> GetProductById(int id)
+        public async Task<ProductResponseDto> GetProductById(int id)
         {
-            var product = await _appDbContext.Products.FindAsync(id);
+            var product = await _appDbContext.Products
+                .AsNoTracking()
+                .Where(p => p.Id == id)
+                .Select(p => new ProductResponseDto(
+                    p.Id,
+                    p.Name,
+                    p.Description,
+                    p.ManufacturerCode,
+                    p.Sku,
+                    p.Quantity,
+                    p.Price,
+                    p.CreatedAt
+                )).FirstOrDefaultAsync();;
 
             return product;
         }
