0545b9e1ebf9de27aa3994bea1c6947c2a445f6b
devdouglasa <douglasalisson27@gmail.com>
2026-03-30 20:31:02 -0300

    feat: add Supplier model and configure relationship in AppDbContext

---
+++ Models/Supplier.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+
+namespace api_dotnet.Models
+{
+    public class Supplier
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string LegalName { get; set; }
+        public string? TradeName { get; set; }
+        [Required]
+        public string NationalTaxId { get; set; } // CNPJ
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+
+        public ICollection<Product> Products { get; set; } = [];
+        public ICollection<StockMovement> StockMovements { get; set; } = [];
+    }
+}
